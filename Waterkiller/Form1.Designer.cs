b4f9730ecb3dfa13a5306a7bf8441f693f0dd438
Albert Hoekman <albert.hoekman@skrepr.com>
2014-09-23 16:06:47 +0200

    images inladen

--- Waterkiller/Form1.Designer.cs
+++ Waterkiller/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.btScan = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btMapSelect = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btScan
@@ -62,16 +66,37 @@
             this.btMapSelect.UseVisualStyleBackColor = true;
             this.btMapSelect.Click += new System.EventHandler(this.btMapSelect_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(376, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.DoubleClick += new System.EventHandler(this.laadIn);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(313, 247);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 332);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btMapSelect);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btScan);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +107,9 @@
         private System.Windows.Forms.Button btScan;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btMapSelect;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Waterkiller/Form1.cs
+++ Waterkiller/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 ﻿using ContourAnalysisNS;
+using Waterkiller;
 
 namespace Waterkiller
 {
@@ -21,12 +23,43 @@ namespace Waterkiller
         private void btScan_Click(object sender, EventArgs e)
         {
             ImageProcessor processor = new ImageProcessor();
-            processor.ProcessImage(Image.FromFile("Geen_Water.jpg");
+            //processor.ProcessImage(Image.FromFile("Geen_Water.jpg");
         }
 
         private void btMapSelect_Click(object sender, EventArgs e)
         {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderBrowser.SelectedPath;
+                loadDirectory(path);
+            }
+        }
 
+        private void loadDirectory(string path)
+        {
+            string[] files = Directory.GetFiles(@path);
+            List<Waterkiller.ListboxItem> formattedFiles = new List<Waterkiller.ListboxItem>();
+            foreach (string file in files)
+            {
+                Waterkiller.ListboxItem entry = new Waterkiller.ListboxItem();
+                entry.Title = Path.GetFileNameWithoutExtension(file);
+                entry.Path = file;
+                formattedFiles.Add(entry);
+            }
+            listBox1.DataSource = formattedFiles;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void laadIn(object sender, EventArgs e)
+        {
+            Waterkiller.ListboxItem selected = (Waterkiller.ListboxItem)listBox1.SelectedItem;
+            pictureBox1.ImageLocation = selected.Path;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
         }
     }
 }
