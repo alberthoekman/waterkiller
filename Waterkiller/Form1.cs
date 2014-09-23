611b3774757779813f9c842609a93d8f201f48bd
ozitiho <bvkoesveld@gmail.com>
2014-09-23 14:46:29 +0200

    Linked both projects

--- Waterkiller/Form1.cs
+++ Waterkiller/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 ﻿using ContourAnalysisNS;
+// ReSharper disable once RedundantUsingDirective
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Waterkiller
 {
@@ -20,8 +23,17 @@ namespace Waterkiller
 
         private void btScan_Click(object sender, EventArgs e)
         {
+            label1.Text = "Running";
             ImageProcessor processor = new ImageProcessor();
-            processor.ProcessImage(Image.FromFile("Geen_Water.jpg");
+            processor.ProcessImage(new Image<Bgr, byte>((Bitmap)Bitmap.FromFile("Geen_Water.jpg")));
+            var foundTemplates = processor.foundTemplates;
+
+            int number = 0;
+            foreach (var foundTemplateDesc in foundTemplates)
+            {
+                number++;
+                label1.Text = number.ToString();
+            }
         }
 
         private void btMapSelect_Click(object sender, EventArgs e)
